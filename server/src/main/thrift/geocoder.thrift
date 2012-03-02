52a0c39bd064900a7fa7ab5c204a31b562a9130a
David Blackman <blackmad@foursquare.com>
2012-03-01 19:32:23 -0500

    thrift server definition

--- server/src/main/thrift/geocoder.thrift
+++ server/src/main/thrift/geocoder.thrift
@@ -6,3 +6,11 @@ struct GeocodeInterpretation {
 struct GeocodeResponse {
   1: list<GeocodeInterpretation> interpretations
 }
+
+struct GeocodeRequest {
+  1: string query
+}
+
+service Geocoder {
+  GeocodeResponse geocode(1: GeocodeRequest r)
+}
