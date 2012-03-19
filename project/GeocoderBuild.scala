4e037b0a9199455de03e0ac9ad54cb2ef601291b
David Blackman <blackmad@foursquare.com>
2012-03-19 16:21:15 -0400

    make publishing work

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -4,6 +4,7 @@ import Keys._
 object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.geocoder",
+    name := "twofish",
     version      := "0.1",
     scalaVersion := "2.9.1"
   )
@@ -15,7 +16,52 @@ object GeocoderBuild extends Build {
     resolvers += "repo.novus snaps" at "http://repo.novus.com/snapshots/",
     resolvers += "Java.net Maven 2 Repo" at "http://download.java.net/maven/2",
     resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-                      "releases"  at "http://oss.sonatype.org/content/repositories/releases")
+                      "releases"  at "http://oss.sonatype.org/content/repositories/releases"),
+
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    publishTo := Some("foursquare Nexus" at "http://nexus.prod.foursquare.com/nexus/content/repositories/releases/"),
+    Keys.publishArtifact in (Compile, Keys.packageDoc) := false,
+
+    pomExtra := (
+      <url>http://github.com/foursquare/twofish</url>
+      <licenses>
+        <license>
+          <name>Apache</name>
+          <url>http://www.opensource.org/licenses/Apache-2.0</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:foursquare/twofish.git</url>
+        <connection>scm:git:git@github.com:foursquare/twofish.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>blackmad</id>
+          <name>David Blackman</name>
+          <url>http://github.com/blackmad</url>
+        </developer>
+      </developers>),
+
+    credentials ++= {
+      val sonatype = ("Sonatype Nexus Repository Manager", "repo.foursquare.com")
+      def loadMavenCredentials(file: java.io.File) : Seq[Credentials] = {
+        xml.XML.loadFile(file) \ "servers" \ "server" map (s => {
+          val host = (s \ "id").text
+          val realm = if (host == sonatype._2) sonatype._1 else "Unknown"
+          Credentials(realm, host, (s \ "username").text, (s \ "password").text)
+        })  
+      }   
+      val ivyCredentials   = Path.userHome / ".ivy2" / ".credentials"
+      val mavenCredentials = Path.userHome / ".m2"   / "settings.xml"
+      (ivyCredentials.asFile, mavenCredentials.asFile) match {
+        case (ivy, _) if ivy.canRead => Credentials(ivy) :: Nil
+        case (_, mvn) if mvn.canRead => loadMavenCredentials(mvn)
+        case _ => Nil
+      }
+    }
   )
 
   lazy val core = Project(id = "core",
