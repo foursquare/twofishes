d5297801d4247a5491a1d2df789df442e2df0af6
David Blackman <blackmad@foursquare.com>
2012-04-02 16:57:46 -0700

    fewer printlns

--- indexer/src/main/scala/importers/osm/OsmParser.scala
+++ indexer/src/main/scala/importers/osm/OsmParser.scala
@@ -15,13 +15,10 @@ class OsmParser extends BinaryParser {
   }
 
   /** Parse a DenseNode protocol buffer and send the resulting nodes to a sink.  */
-  override protected def parseDense(nodes: Osmformat.DenseNodes) {
-    println(nodes)
-  }
+  override protected def parseDense(nodes: Osmformat.DenseNodes) {}
   /** Parse a list of Node protocol buffers and send the resulting nodes to a sink.  */
   override protected def parseNodes(nodes: java.util.List[Osmformat.Node]) {
     nodes.foreach(node => {
-      println
       0.to(node.getKeysCount()).foreach(kIndex => {
         val key = getStringById(node.getKeys(kIndex));
         val value = getStringById(node.getVals(kIndex));
@@ -30,13 +27,9 @@ class OsmParser extends BinaryParser {
     })
   }
   /** Parse a list of Way protocol buffers and send the resulting ways to a sink.  */
-  override protected def parseWays(ways: java.util.List[Osmformat.Way]) {
-    println(ways)
-  }
+  override protected def parseWays(ways: java.util.List[Osmformat.Way]) { }
   /** Parse a header message. */
-  override protected def parse(header: Osmformat.HeaderBlock) {
-    println(header)
-  }
+  override protected def parse(header: Osmformat.HeaderBlock) { }
 
   protected def complete() {}
 }
