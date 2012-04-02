d6a3166fb0a8068f1722e2801d84ae0185342475
David Blackman <blackmad@foursquare.com>
2012-04-02 16:47:01 -0700

    start of OSM

---
+++ indexer/src/main/scala/importers/osm/OsmParser.scala
@@ -0,0 +1,31 @@
+import crosby.binary._
+import crosby.binary.file.BlockInputStream
+import scala.collection.JavaConversions._
+import java.io._
+
+class OsmParser extends BinaryParser {
+  def doParse(filename: String) {
+    val fis = new FileInputStream(filename)
+    val bis = new BlockInputStream(fis, this)
+  }
+
+  protected def parseRelations(rels: java.util.List[Osmformat.Relation]) {}
+  /** Parse a DenseNode protocol buffer and send the resulting nodes to a sink.  */
+  protected def parseDense(nodes: Osmformat.DenseNodes) {}
+  /** Parse a list of Node protocol buffers and send the resulting nodes to a sink.  */
+  protected def parseNodes(nodes: java.util.List[Osmformat.Node]) {
+    nodes.foreach(node => {
+      0.to(node.getKeysCount()).foreach(kIndex => {
+        val key = getStringById(node.getKeys(kIndex));
+        val value = getStringById(node.getVals(kIndex));
+        println("%s %s".format(key, value))
+      })
+    })
+  }
+  /** Parse a list of Way protocol buffers and send the resulting ways to a sink.  */
+  protected def parseWays(ways: java.util.List[Osmformat.Way]) {}
+  /** Parse a header message. */
+  protected def parse(header: Osmformat.HeaderBlock) {}
+
+  protected def complete() {}
+}
