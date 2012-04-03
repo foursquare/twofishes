badc79607952b78781f4c017a39742d3b5acffcd
David Blackman <blackmad@foursquare.com>
2012-04-02 19:44:41 -0700

    dense parser

--- indexer/src/main/scala/importers/osm/OsmParser.scala
+++ indexer/src/main/scala/importers/osm/OsmParser.scala
@@ -14,7 +14,32 @@ class OsmParser extends BinaryParser {
   }
 
   /** Parse a DenseNode protocol buffer and send the resulting nodes to a sink.  */
-  override protected def parseDense(nodes: Osmformat.DenseNodes) {}
+  override protected def parseDense(nodes: Osmformat.DenseNodes) {
+    var lastId: Long = 0
+    var lastLat: Long = 0
+    var lastLon: Long = 0
+ 
+    0.until(nodes.getIdCount()).foreach(nid => {
+      val lat = nodes.getLat(nid) + lastLat
+      val lon = nodes.getLon(nid) + lastLon
+      val id = nodes.getId(nid) + lastId
+      lastLat = lat
+      lastLon = lon
+      lastId = id
+
+      println("%s @ %s,%s".format(id, lat, lon))
+
+      0.until(nodes.getKeysValsCount(), 2).foreach(kvid => {
+        val keyid = nodes.getKeysVals(kvid)
+        val valid = nodes.getKeysVals(kvid + 1)
+        val key = getStringById(keyid)
+        val value = getStringById(valid)
+        println("%s: %s".format(key, value))
+      })
+    })
+  }
+
+
   /** Parse a list of Node protocol buffers and send the resulting nodes to a sink.  */
   override protected def parseNodes(nodes: java.util.List[Osmformat.Node]) {
     nodes.foreach(node => {
