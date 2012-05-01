895bf2cf7b27e1d4259f676e7208ecbef39462e4
David Blackman <blackmad@foursquare.com>
2012-04-30 23:35:16 -0400

    script to pull out only buildings with links from the input data

---
+++ extract-wiki-buildings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+
+try:
+  os.mkdir("data/downloaded")
+except:
+  pass
+
+alternateNames = open("data/downloaded/alternateNames.txt")
+input = open("data/downloaded/allCountries.txt")
+output = open("data/supplemental/buildings.txt", "w")
+
+gidList = set()
+
+for line in alternateNames:
+  line.strip
+  parts = line.split('\t')
+  gid = parts[1]
+  lang = parts[2]
+  if lang == 'link':
+    gidList.add(gid)
+
+for line in input:
+  parts = line.split('\t')
+  if parts[0] in gidList:
+    output.write(line)
+
