c367c1ee87e461c82c9211ba2c8d70c8755fe5b0
David Blackman <blackmad@foursquare.com>
2012-09-13 09:56:59 -0400

    new hfile output
    geoid -> feature with parent ids fixed so no side-lookup table is needed
    
    (not for geocoder serving (yet?))

--- indexer/src/main/scala/OutputHFile.scala
+++ indexer/src/main/scala/OutputHFile.scala
@@ -15,7 +15,7 @@ import java.util.Arrays
 import org.apache.hadoop.conf.Configuration 
 import org.apache.hadoop.fs.{LocalFileSystem, Path}
 import org.apache.hadoop.hbase.KeyValue.KeyComparator
-import org.apache.hadoop.hbase.io.hfile.{CacheConfig, Compression, HFile, HFileScanner, HFileWriterV2}
+import org.apache.hadoop.hbase.io.hfile.{CacheConfig, Compression, HFile, HFileScanner, HFileWriterV1, HFileWriterV2}
 import org.apache.hadoop.hbase.util.Bytes._
 
 import org.apache.thrift.TSerializer
@@ -34,16 +34,27 @@ class OutputHFile(basepath: String) {
   val conf = new Configuration();
   val cconf = new CacheConfig(conf);
 
+  def buildV2Writer(filename: String) = {
+    val fs = new LocalFileSystem() 
+    val path = new Path(new File(basepath, filename).toString)
+    fs.initialize(URI.create("file:///"), conf)
+    new HFileWriterV2(conf, cconf, fs, path, blockSize, compressionAlgo, null)
+  }
+
+  def buildV1Writer(filename: String) = {
+    val fs = new LocalFileSystem() 
+    val path = new Path(new File(basepath, filename).toString)
+    fs.initialize(URI.create("file:///"), conf)
+    new HFileWriterV1(conf, cconf, fs, path, blockSize, compressionAlgo, null)
+  }
+  
   def writeCollection[T <: AnyRef, K <: Any](
     filename: String,
     callback: (T) => (Array[Byte], Array[Byte]),
     dao: SalatDAO[T, K],
     sortField: String
   ) {
-    val fs = new LocalFileSystem() 
-    val path = new Path(new File(basepath, filename).toString)
-    fs.initialize(URI.create("file:///"), conf)
-    val writer = new HFileWriterV2(conf, cconf, fs, path, blockSize, compressionAlgo, null)
+    val writer = buildV2Writer(filename)
     var fidCount = 0
     val fidSize = dao.collection.count
     val fidCursor = dao.find(MongoDBObject())
@@ -53,14 +64,18 @@ class OutputHFile(basepath: String) {
       writer.append(k, v)
       fidCount += 1
       if (fidCount % 1000 == 0) {
-        println("processed %d of %d %s".format(fidCount, fidSize, path))
+        println("processed %d of %d %s".format(fidCount, fidSize, filename))
       }
     })
     writer.close()
   }
 
+  val comp = new ByteArrayComparator()
+  def lexicalSort(a: Array[Byte], b: Array[Byte]) = {
+    comp.compare(a, b) < 0
+  }
+
   def writeNames() {
-    val comp = new ByteArrayComparator()
     val nameMap = new HashMap[Array[Byte], List[String]]
     var nameCount = 0
     val nameSize = NameIndexDAO.collection.count
@@ -73,17 +88,11 @@ class OutputHFile(basepath: String) {
       }
     })
 
-    val fpath = new File(basepath, "name_index.hfile").toString
-    val fs = new LocalFileSystem() 
-    val path = new Path(fpath)
-    fs.initialize(URI.create("file:///"), conf)
-    val writer = new HFileWriterV2(conf, cconf, fs, path, blockSize, compressionAlgo, null)
+    val writer = buildV2Writer("name_index.hfile")
 
     println("sorting")
 
-    val sortedMap = nameMap.keys.toList.sort((a, b) => {
-        comp.compare(a, b) < 0
-    })
+    val sortedMap = nameMap.keys.toList.sort(lexicalSort)
 
     println("sorted")
 
@@ -105,8 +114,10 @@ class OutputHFile(basepath: String) {
 
   type IdFixer = (String) => Option[String]
 
+  val factory = new TBinaryProtocol.Factory()
+
   def serializeBytes(g: GeocodeRecord, fixParentId: IdFixer) = {
-    val serializer = new TSerializer(new TBinaryProtocol.Factory());
+    val serializer = new TSerializer(factory);
     val f = g.toGeocodeServingFeature()
     val parentOids = f.scoringFeatures.parents.flatMap(f => fixParentId(f))
     f.scoringFeatures.setParents(parentOids)
@@ -137,6 +148,10 @@ class OutputHFile(basepath: String) {
       MongoGeocodeDAO, "_id")
   }
 
+  val ThriftClassValueBytes: Array[Byte] = "value.thrift.class".getBytes("UTF-8")
+  val ThriftClassKeyBytes: Array[Byte] = "key.thrift.class".getBytes("UTF-8")
+  val ThriftEncodingKeyBytes: Array[Byte] = "thrift.protocol.factory.class".getBytes("UTF-8")
+
   def processForGeoId() {
     val geoCursor = MongoGeocodeDAO.find(MongoDBObject())
 
@@ -145,6 +160,9 @@ class OutputHFile(basepath: String) {
     }
     
     val gidMap = new HashMap[String, String]
+
+    val ids = MongoGeocodeDAO.find(MongoDBObject()).map(pickBestId)
+      .map(_.getBytes("UTF-8")).toList.sort(lexicalSort)
 
     geoCursor.foreach(g => {
       if (g.ids.size > 1) {
@@ -159,11 +177,26 @@ class OutputHFile(basepath: String) {
 
     def fixParentId(fid: String) = Some(gidMap.getOrElse(fid, fid))
 
-    writeCollection("gid-features.hfile",
-      (g: GeocodeRecord) => 
-        (pickBestId(g).getBytes("UTF-8"), serializeBytes(g, fixParentId)),
-      MongoGeocodeDAO, "_id")
+    val filename = "gid-features.hfile"
+    val writer = buildV1Writer(filename)
+    writer.appendFileInfo(ThriftClassValueBytes,
+      classOf[GeocodeServingFeature].getName.getBytes("UTF-8"))
+    writer.appendFileInfo(ThriftEncodingKeyBytes,
+      factory.getClass.getName.getBytes("UTF-8"))
+
+    var fidCount = 0
+    val fidSize = ids.size
+    ids.grouped(2000).foreach(chunk => {
+      val records = MongoGeocodeDAO.find(MongoDBObject("ids" -> MongoDBObject("$in" -> chunk)))
+      records.foreach(g => {
+        val (k, v) =
+          (pickBestId(g).getBytes("UTF-8"), serializeBytes(g, fixParentId))
+        writer.append(k, v)
+        fidCount += 1
+        if (fidCount % 1000 == 0) {
+          println("processed %d of %d %s".format(fidCount, fidSize, filename))
+        }
+      })
+    })
   }
-
-
 }
