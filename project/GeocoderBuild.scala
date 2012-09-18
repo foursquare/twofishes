fe5ac0d528aff40c3f26f9c7efc6cef7d001b6de
David Blackman <blackmad@foursquare.com>
2012-09-18 15:10:42 +0000

    bump version 0.37

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,7 +7,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.36",
+    version      := "0.37",
     scalaVersion := "2.9.1"
   )
 
