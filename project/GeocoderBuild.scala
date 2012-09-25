49f24942592c59022d34632b5d0a759fa012e234
David Blackman <blackmad@foursquare.com>
2012-09-25 20:29:02 +0000

    bump version, include nyc neighborhood aliases

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,7 +7,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.37",
+    version      := "0.38",
     scalaVersion := "2.9.1"
   )
 
