b328fc9b783d1dbb64e52f7be20fa19e6ac77be3
David Blackman <blackmad@foursquare.com>
2012-10-31 15:39:18 -0400

    down redownload sbt jar

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
-curl -o sbt-launch.jar http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/0.11.2/sbt-launch.jar
+if [ ! -f sbt-launch.jar ];
+then
+  curl -o sbt-launch.jar http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/0.11.2/sbt-launch.jar
+fi
