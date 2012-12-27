784f1b0a8d77a35c592b49d5cfa5059b7cc073f4
David Blackman <blackmad@foursquare.com>
2012-12-27 16:27:28 -0500

    make sure developer has thrift-finagle-0.5.0

--- update-thrift.sh
+++ update-thrift.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+if thrift -version | grep -v 0.5.0 > /dev/null; then
+  echo "You need thrift compiler version 0.5.0, with finagle extensions"
+  echo "see: https://github.com/mariusaeriksen/thrift-0.5.0-finagle"
+  exit 1
+fi
+
+if ! grep finagle `which thrift` > /dev/null; then
+  echo "You don't have thrift with finagle"
+  echo "see: https://github.com/mariusaeriksen/thrift-0.5.0-finagle"
+  exit 1
+fi
+
+
 thrift --gen java -o interface/src/main/java interface/src/main/thrift/geocoder.thrift
