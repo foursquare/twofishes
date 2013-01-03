6e2baef1cc6afcd2e6c89145163063b4f7b336d3
David Blackman <blackmad@foursquare.com>
2013-01-02 23:34:01 -0500

    missing files

---
+++ util/src/main/scala/Hacks.scala
@@ -0,0 +1,12 @@
+// Copyright 2012 Foursquare Labs Inc. All Rights Reserved.
+package com.foursquare.twofishes.util
+
+object Hacks {
+  val nycBoroughIds = List(
+    "US-NY-061", // manhattan
+    "US-NY-085", // staten island
+    "US-NY-005", // bronx
+    "US-NY-081", // queens,
+    "US-NY-047" // brooklyn
+  )
+}
