14b73e65702cd6a4fa3701ed715b190ca98852a9
David Blackman <blackmad@foursquare.com>
2013-01-03 21:59:43 +0000

    up sbt's memory, up version #

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,7 +7,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.52",
+    version      := "0.53",
     scalaVersion := "2.9.1"
   )
 
