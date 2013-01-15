91ceb1ce0e033127ff799c5ec2bc8642db31f3d6
David Blackman <blackmad@foursquare.com>
2013-01-15 17:03:43 -0500

    csv reader was totally failing me

--- scripts/extract-adm.py
+++ scripts/extract-adm.py
@@ -7,13 +7,15 @@ csv.field_size_limit(1000000000)
 
 if len(sys.argv) == 2:
   cc = sys.argv[1]
-  reader = csv.reader(open('data/downloaded/%s.txt' % cc), dialect='excel-tab')
+  reader = open('data/downloaded/%s.txt' % cc)
   writer = csv.writer(open('data/computed/adminCodes-%s.txt' % cc, 'w'), dialect='excel-tab')
 else:
-  reader = csv.reader(open('data/downloaded/allCountries.txt'), dialect='excel-tab')
+  reader = open('data/downloaded/allCountries.txt')
   writer = csv.writer(open('data/computed/adminCodes.txt', 'w'), dialect='excel-tab')
 
-for row in reader:
+index = 0
+for line in reader:
+  row = line.split('\t')
   geonameid = row[0]
   name = row[1]
   asciiname = row[2]
@@ -23,13 +25,17 @@ for row in reader:
   adminIds = [countryCode] + row[10:14]
   adminIds = [a for a in adminIds if a]
 
+  index += 1 
+
   validFeatureCodes = ['ADM1', 'ADM2', 'ADM3', 'ADM4']
 
   if fclass == 'A' and fcode in validFeatureCodes:
     adminLevel = int(fcode[3])
     adminId = '.'.join(adminIds)
+
     if adminLevel != (len(adminIds) - 1):
-      print "didn't have enough adminids for %s %s @ %s %s" % (geonameid, name, fcode, adminId)
+      #print "didn't have enough adminids for %s %s @ %s %s" % (geonameid, name, fcode, adminId)
+      pass
     else:
       outputRow = [adminId, name, asciiname, geonameid]
       writer.writerow(outputRow)
