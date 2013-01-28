1292f78587d11a3f2828cac79967020fece08934
David Blackman <blackmad@foursquare.com>
2013-01-27 21:23:40 -0500

    optimization for "full" cells, I hope

--- interface/src/main/thrift/side_tables.thrift
+++ interface/src/main/thrift/side_tables.thrift
@@ -12,3 +12,14 @@ struct ChildEntry {
 struct ChildEntries {
   1: optional list<ChildEntry> entries
 }
+
+struct CellGeometry {
+  1: optional binary oid,
+  2: optional binary wkbGeometry,
+  3: optional geocoder.YahooWoeType woeType,
+  4: optional bool full
+}
+
+struct CellGeometries {
+  1: optional list<CellGeometry> cells
+}
