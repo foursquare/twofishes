2b03a597f6e814c08f16492f3ea2284159102dd0
David Blackman <blackmad@foursquare.com>
2013-02-19 15:59:32 +0000

    doesn't like extra point

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,7 +7,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.74.4.1",
+    version      := "0.74.5",
     scalaVersion := "2.9.1"
   )
 
