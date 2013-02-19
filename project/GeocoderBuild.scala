2f190ba6d0806c7eaa0b0a98128012072cbe3234
David Blackman <blackmad@foursquare.com>
2013-02-19 15:45:27 +0000

    fix geocoder.html display name on revgeo

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,7 +7,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.74.3",
+    version      := "0.74.4",
     scalaVersion := "2.9.1"
   )
 
