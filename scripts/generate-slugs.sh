04d702a8098228d25a83d7de91ca5ef44e207654
David Blackman <blackmad@foursquare.com>
2013-02-20 02:18:51 +0000

    try to fix missing slug script. fail.

--- scripts/generate-slugs.sh
+++ scripts/generate-slugs.sh
@@ -1,9 +1,12 @@
 echo "
 import com.mongodb.casbah.Imports._
+import com.foursquare.twofishes._
+import com.foursquare.twofishes.importers.geonames._
+
 MongoGeocodeDAO.find(MongoDBObject()).foreach(f => {
 if (List(YahooWoeType.TOWN, YahooWoeType.SUBURB, YahooWoeType.COUNTRY, YahooWoeType.ADMIN1, YahooWoeType.ADMIN2).contains(f.woeType)) {
   val id = f.ids.find(_.startsWith(\"geonameid\")).getOrElse(f.ids(0))
-  GeonamesParser.missingSlugList.add(id)
+  GeonamesParser.slugIndexer.missingSlugList.add(id)
 }
 })
-GeonamesParser.buildMissingSlugs" | ./sbt indexer/console > output.txt
+GeonamesParser.slugIndexer.buildMissingSlugs" | ./sbt indexer/console > output.txt
