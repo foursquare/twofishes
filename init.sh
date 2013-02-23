e74aa069b4333f6fa3686c967f2514d4a9be27fd
David Blackman <blackmad@foursquare.com>
2013-02-23 16:53:49 +0000

    refresh init.sh for continuous build

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-if [ ! -f sbt-launch.jar ];
-then
-  curl -o sbt-launch.jar http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.12.2/sbt-launch.jar
-fi
+rm sbt-launch.jar
+curl -o sbt-launch.jar http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.12.2/sbt-launch.jar
