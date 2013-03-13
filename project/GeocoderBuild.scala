ce009c581e2e092fb841ba027c0ad55959ba3583
Neil Sanchala <nsanch@gmail.com>
2013-03-13 22:03:46 +0000

    0.74.11

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,7 +7,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.74.10",
+    version      := "0.74.11",
     scalaVersion := "2.9.1"
   )
 
