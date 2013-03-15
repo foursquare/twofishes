b34c254ac06f7a04249fbd080d4c918e62a56a45
Neil Sanchala <nsanch@gmail.com>
2013-03-15 21:31:43 +0000

    0.74.13
    i would explain why this is necessary but the pain is too fresh, i don't
    want to talk about it

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,7 +7,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.74.12",
+    version      := "0.74.13",
     scalaVersion := "2.9.1"
   )
 
