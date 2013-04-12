123c5c759c488922e000f6dcfc9ddb97a7352c55
David Blackman <blackmad@foursquare.com>
2013-04-12 15:12:22 -0400

    it's a slow query log

--- server/src/main/scala/GeocodeServer.scala
+++ server/src/main/scala/GeocodeServer.scala
@@ -22,6 +22,99 @@ import org.jboss.netty.buffer.ChannelBuffers
 import org.jboss.netty.handler.codec.http._
 import org.jboss.netty.util.CharsetUtil
 import scala.collection.mutable.ListBuffer
+import java.util.concurrent.ConcurrentHashMap
+import org.bson.types.ObjectId
+import java.util.Date
+
+
+class CircularBufferIterator[T](buffer:Array[T], start:Int) extends Iterator[T]{
+  var idx = 0
+  override def hasNext = idx < buffer.size
+  override def next() = {
+    val i=idx
+    idx=idx+1
+    buffer(i)
+  } 
+}
+ 
+class CircularBuffer[T](size:Int)(implicit m:Manifest[T]) extends Seq[T]{
+  val buffer=new Array[T](size);
+  var bIdx=0;
+  
+  override def apply (idx: Int): T = buffer((bIdx+idx) % size)
+ 
+  override def length = size
+ 
+  override def iterator= new CircularBufferIterator[T](buffer, bIdx)
+ 
+  def add(e:T)= {
+    buffer(bIdx)=e
+    bIdx=(bIdx +1) % size
+  }
+}
+
+class QueryLogHttpHandler(
+  queryMap: ConcurrentHashMap[ObjectId, (GeocodeRequest, Long)],
+  slowQueries: Seq[(GeocodeRequest, Long, Long)]
+) extends Service[HttpRequest, HttpResponse] {
+  def apply(request: HttpRequest) = {
+    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+    val currentTime = System.nanoTime
+
+    val content = (queryMap.asScala.map({case (k, v) => {
+      "Request has taken %llns so far\n%s".format(k, currentTime - v._2, v._1)
+    }}).mkString("\n") + "-----------------------------------------\n" + "SLOW QUERIES\n"
+      + slowQueries.map({case ((req, start, end)) => {
+        "Query took %d ns, %d ms --  started at %s, ended at %s\n%s".format(
+          end - start,
+          (end - start) / 1000000,
+          new Date(start / 1000000),
+          new Date(end / 1000000)
+        )
+      }}).mkString("\n")    )
+
+    response.setHeader("Content-Type", "text/plain")
+    response.setContent(ChannelBuffers.copiedBuffer(content, CharsetUtil.UTF_8))
+    Future.value(response)
+  }
+}
+
+class QueryLoggingGeocodeServerImpl(service: Geocoder.ServiceIface) extends Geocoder.ServiceIface {
+  val queryMap = new ConcurrentHashMap[ObjectId, (GeocodeRequest, Long)]
+  val slowQueries = new CircularBuffer[(GeocodeRequest, Long, Long)](1000)
+
+  val slowQueryHttpHandler = new QueryLogHttpHandler(queryMap, slowQueries)
+
+  def queryLogProcessor(r: GeocodeRequest, f: (GeocodeRequest => Future[GeocodeResponse])): Future[GeocodeResponse] = {
+    // log the start of this query
+    val start = System.nanoTime
+    val id = new ObjectId()
+    queryMap.put(id, (r, start))
+
+    def logCompletion() {
+      val end = System.nanoTime
+      // greater than 1 second 
+      if (end - start > 1000000000L) {
+        // log slow query
+        println("%s took %dns %d ms".format(r, end - start, (end - start) / 1000000))
+        slowQueries.add((r, start, end))
+      }
+      queryMap.remove(id)
+    }
+
+    f(r) onSuccess { resp => 
+      logCompletion()
+      resp
+    }
+  }
+
+  def geocode(r: GeocodeRequest): Future[GeocodeResponse] =
+    queryLogProcessor(r, service.geocode)
+
+  def reverseGeocode(r: GeocodeRequest): Future[GeocodeResponse] =
+    queryLogProcessor(r, service.reverseGeocode)
+
+}
 
 class GeocodeServerImpl(store: GeocodeStorageReadService) extends Geocoder.ServiceIface {
   val queryFuturePool = FuturePool(StatsWrappedExecutors.create(24, 100, "geocoder"))
@@ -48,7 +141,7 @@ class HandleExceptions extends SimpleFilter[HttpRequest, HttpResponse] {
   }
 }
 
-class GeocoderHttpService(geocoder: GeocodeServerImpl) extends Service[HttpRequest, HttpResponse] {
+class GeocoderHttpService(geocoder: Geocoder.ServiceIface) extends Service[HttpRequest, HttpResponse] {
   val diskIoFuturePool = FuturePool(Executors.newFixedThreadPool(8))
 
   def handleGeocodeQuery(request: GeocodeRequest) = 
@@ -226,7 +319,7 @@ object GeocodeFinagleServer {
     val config = new GeocodeServerConfig(args)
 
     // Implement the Thrift Interface
-    val processor = new GeocodeServerImpl(ServerStore.getStore(config))
+    val processor = new QueryLoggingGeocodeServerImpl(new GeocodeServerImpl(ServerStore.getStore(config)))
 
     // Convert the Thrift Processor to a Finagle Service
     val service = new Geocoder.Service(processor, new TBinaryProtocol.Factory())
@@ -234,6 +327,7 @@ object GeocodeFinagleServer {
     println("serving finagle-thrift on port %d".format(config.thriftServerPort))
     println("serving http/json on port %d".format(config.thriftServerPort + 1))
     println("serving debug info on port %d".format(config.thriftServerPort + 2))
+    println("serving slow query http/json on port %d".format(config.thriftServerPort + 3))
 
     val server: Server = ServerBuilder()
       .bindTo(new InetSocketAddress(config.thriftServerPort))
@@ -259,5 +353,12 @@ object GeocodeFinagleServer {
         .reportTo(new FoursquareStatsReceiver)
         .build(handleExceptions andThen new GeocoderHttpService(processor))
     }
+
+    ServerBuilder()
+      .bindTo(new InetSocketAddress(config.thriftServerPort + 3))
+      .codec(Http())
+      .name("geocoder-slow-query-http")
+      .reportTo(new FoursquareStatsReceiver)
+      .build(handleExceptions andThen processor.slowQueryHttpHandler)
   }
 }
