890ed5490110f18b9045d7500d0a4c7fd56779af
Neil Sanchala <nsanch@foursquare.com>
2013-08-26 16:49:21 -0400

    hack for less contention in mapfile reading
    - GeocodeRecordMapFileInput is a source of contention because reading
      from a MapFile holds a lock. As a short-term solution, read that in 4
      times and put the 4 instances in a queue so we get some more
      throughput. HACK.
    - Timer around time of mapfile read, so we can verify that spikes of
      overall time are correlated to spikes of that time. (If they are, it
      doesn't necessarily mean this is the cause, but it's a signal.) Also
      log the keys and value sizes, just in case there's some sort of
      pattern.
    - Also time the time to get a mapfile so we can measure contention that
      way.
    - Separate most of MMapInputStream into an Impl class that can safely be
      shared by multiple MMapInputStream's. That way, we can only do one set
      of mmap's per file. Java was OOM'ing without this. (Which is
      surprising since I'm running on 64-bit, actually.)

--- core/src/main/scala/MMapInputStream.scala
+++ core/src/main/scala/MMapInputStream.scala
@@ -33,14 +33,11 @@ class ByteBufferReader(val buf: ByteBuffer) {
   }
 }
 
-/** A FSDataInputStream-compatible InputStream that uses mmap under the covers
-  * to be faster. All methods are thread-safe, but the methods that assume a
-  * shared position will have worse concurrency because there will be a per-file
-  * lock on the position. If possible, use readFully and the read() overload
-  * that accepts a position, because those don't acquire a lock until you get
-  * down to the individual memory-mapped blocks. */
-class MMapInputStream(filename: String, mmapChunkSize: Int = 64*1024)
-    extends InputStream with Seekable with PositionedReadable {
+/** Positionless implementation for MMapInputStream that uses mmap under the
+  * covers to be faster. All methods are thread-safe and can be used by
+  * multiple MMapInputStream's so we can have multiple readers hitting the same
+  * input stream, sharing the same mmap'd memory. */
+class MMapInputStreamImpl(filename: String, mmapChunkSize: Int = 64*1024) {
   val (fileChannel, readers, fileSize) = {
     val file: File = new File(filename)
     val fileChannel: FileChannel = (new FileInputStream(file)).getChannel
@@ -55,8 +52,6 @@ class MMapInputStream(filename: String, mmapChunkSize: Int = 64*1024)
     (fileChannel, readers, fileSize)
   }
 
-  private var activePosition: Long = 0
-
   private def getStream(pos: Long): Option[(ByteBufferReader, Int)] = {
     val index = (pos / mmapChunkSize).toInt
     if (index < readers.length) {
@@ -70,40 +65,22 @@ class MMapInputStream(filename: String, mmapChunkSize: Int = 64*1024)
 
   def preload = readers.foreach(_.preload)
 
-  override def close = fileChannel.close
-  override def read(): Int = synchronized {
-    getStream(activePosition) match {
+  def close = fileChannel.close
+
+  def readByteAtPosition(pos: Long): (Int, Long) = {
+    getStream(pos) match {
       case Some((reader, posInStream)) =>
         val result = reader.read(posInStream)
         if (result >= 0) {
-          activePosition += 1
+          (result, pos + 1)
+        } else {
+          (result, pos)
         }
-        result
-      case None => -1
+      case None => (-1, pos)
     }
-  }
-  override final def read(buffer: Array[Byte], off: Int, len: Int): Int = synchronized {
-    val bytesRead = read(activePosition, buffer, off, len)
-    if (bytesRead > 0) {
-      activePosition += bytesRead
-    }
-    bytesRead
-  }
-  override def getPos: Long = synchronized {
-    activePosition
-  }
-  override def seek(pos: Long) = synchronized {
-    if (pos > fileSize) {
-      throw new IOException("can't seek past end of file")
-    }
-    activePosition = pos
-  }
-  override def seekToNewSource(targetPos: Long): Boolean = {
-    // Same as what RawLocalFileSystem's impl does.
-    false
   }
 
-  override def read(pos: Long, buffer: Array[Byte], offset: Int, length: Int): Int = {
+  def read(pos: Long, buffer: Array[Byte], offset: Int, length: Int): Int = {
     var soFar = 0
     var exhausted = false
     while (!exhausted && soFar < length) {
@@ -119,6 +96,68 @@ class MMapInputStream(filename: String, mmapChunkSize: Int = 64*1024)
       }
     }
     soFar
+  }
+}
+
+object MMapInputStreamImpl {
+  var streams: Map[String, MMapInputStreamImpl] = Map.empty
+
+  def findOrOpen(filename: String) = synchronized {
+    streams.get(filename).getOrElse({
+      val ret = new MMapInputStreamImpl(filename)
+      streams = streams.updated(filename, ret)
+      ret
+    })
+  }
+}
+
+/** A FSDataInputStream-compatible InputStream that uses mmap under the covers
+  * to be faster. All methods are thread-safe, but the methods that assume a
+  * shared position will have worse concurrency because there will be a per-file
+  * lock on the position. If possible, use readFully and the read() overload
+  * that accepts a position, because those don't acquire a lock until you get
+  * down to the individual memory-mapped blocks.
+  *
+  * This uses MMapInputStreamImpl for the actual implementation so multiple
+  * people can read the same file and share the same mmap'd data.
+  */
+class MMapInputStream(filename: String) extends InputStream with Seekable with PositionedReadable {
+
+  private val impl = MMapInputStreamImpl.findOrOpen(filename)
+
+  private var activePosition: Long = 0
+
+  def preload = impl.preload
+
+  override def close = impl.close
+  override def read(): Int = synchronized {
+    val (result, newPos) = impl.readByteAtPosition(activePosition)
+    activePosition = newPos
+    result
+  }
+  override final def read(buffer: Array[Byte], off: Int, len: Int): Int = synchronized {
+    val bytesRead = read(activePosition, buffer, off, len)
+    if (bytesRead > 0) {
+      activePosition += bytesRead
+    }
+    bytesRead
+  }
+  override def getPos: Long = synchronized {
+    activePosition
+  }
+  override def seek(pos: Long) = synchronized {
+    if (pos > impl.fileSize) {
+      throw new IOException("can't seek past end of file")
+    }
+    activePosition = pos
+  }
+  override def seekToNewSource(targetPos: Long): Boolean = {
+    // Same as what RawLocalFileSystem's impl does.
+    false
+  }
+
+  override def read(pos: Long, buffer: Array[Byte], offset: Int, length: Int): Int = {
+    impl.read(pos, buffer, offset, length)
   }
 
   override def readFully(pos: Long, buffer: Array[Byte]) = {
