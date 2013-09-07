d111a317a9550999de2626e7954be376533efda1
David Blackman <blackmad@foursquare.com>
2013-09-06 18:21:42 -0700

    Revert "rely on spindle from git for now"
    This reverts commit 70653130c64fa4ee8cf8d7fa132d70f643f99055.

--- project/project/build.scala
+++ project/project/build.scala
@@ -2,8 +2,7 @@ import sbt._
 
 object Plugins extends Build {
   lazy val root = Project("root", file(".")) dependsOn(
-    uri("http://github.com/foursquare/spindle.git"),
-    uri("http://github.com/sbt/sbt-assembly.git#0.9.2")
+    uri("git://github.com/alno/sbt-caliper.git")
   )
 }
 
