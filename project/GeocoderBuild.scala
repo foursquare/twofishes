7e54bb823be3bb368a778ebc3423db5bf197a2ec
David Blackman <blackmad@foursquare.com>
2013-09-10 20:02:27 +0000

    target 1.6

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -7,8 +7,10 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.76.5.2_b",
-    crossScalaVersions := Seq("2.9.2", "2.10.2")
+    version      := "0.76.5.2_c",
+    crossScalaVersions := Seq("2.9.2", "2.10.2"),
+    javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
+    javacOptions in doc := Seq("-source", "1.6")
   )
 
   lazy val scoptSettings = Seq(
