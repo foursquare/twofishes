dd3e2e7993ca1e4635d776350d2f36efd0aafd15
Rahul Maddimsetty <rahul@foursquare.com>
2014-01-27 22:35:21 +0000

    Bumping up twofishes version

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -9,7 +9,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.80.5s",
+    version      := "0.80.5t",
     scalaVersion := "2.10.2",
     crossScalaVersions := Seq("2.9.2", "2.10.2"),
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
