61b3b3fa74d4d363e2c65e69ce60e574e1b4e691
David Blackman <blackmad@foursquare.com>
2014-04-28 10:07:30 -0400

    82.13, revert punct change

--- project/GeocoderBuild.scala
+++ project/GeocoderBuild.scala
@@ -9,7 +9,7 @@ object GeocoderBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.foursquare.twofishes",
     name := "twofishes",
-    version      := "0.82.12",
+    version      := "0.82.13",
     scalaVersion := "2.10.2",
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
     javacOptions in doc := Seq("-source", "1.6")
