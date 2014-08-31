4341183d8d557b42a535515ba4d2c0357a4cb6ad
Son Pham <son.pham@ocado.com>
2014-08-31 15:29:57 +0100

    Charset.defaultCharset() is not a String

--- server/src/main/scala/GeocodeServer.scala
+++ server/src/main/scala/GeocodeServer.scala
@@ -68,7 +68,8 @@ class QueryLogHttpHandler(
 }
 
 class QueryLoggingGeocodeServerImpl(service: Geocoder.ServiceIface) extends Geocoder.ServiceIface with Logging {
-  if (Charset.defaultCharset() != "UTF-8") {
+  println("Current default charset is: " + Charset.defaultCharset())
+  if (Charset.defaultCharset() != Charset.forName("UTF-8")) {
     throw new Exception("Default charset is not utf-8, this server probably won't work. see: http://perlgeek.de/en/article/set-up-a-clean-utf8-environment")
   }
 
