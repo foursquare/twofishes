7671b945116c5f4ddfecf7599a1e97ab78833ad9
Jon Parise <jon@pinterest.com>
2014-10-31 12:36:47 -0700

    Verify that the hotfix directory actually exists.
    Otherwise, if an invalid hotfile path was supplied, we'd die with a
    NullPointerException deep in the JSON-to-Thrift deserializer.

--- server/src/main/scala/JsonHotfixSource.scala
+++ server/src/main/scala/JsonHotfixSource.scala
@@ -3,23 +3,27 @@ package com.foursquare.twofishes
 
 import com.foursquare.common.thrift.json.TReadableJSONProtocol
 import java.io.File
+import com.weiglewilczek.slf4s.Logging
 import org.apache.thrift.TDeserializer
 
-class JsonHotfixSource(originalPath: String) extends HotfixSource {
+class JsonHotfixSource(originalPath: String) extends HotfixSource with Logging {
   val deserializer = new TDeserializer(new TReadableJSONProtocol.Factory())
   var path = ""
   var allEdits: Seq[GeocodeServingFeatureEdit] = Seq()
 
   def init() {
     // reread canonical path in case symlink was modified between refreshes
-    path = new File(originalPath).getCanonicalPath()
+    path = new File(originalPath).getCanonicalPath
     val dir = new File(path)
-    allEdits = dir.listFiles.filter(f => f.getName.endsWith(".json")).flatMap(file => {
-      val edits = new RawGeocodeServingFeatureEdits
-      deserializer.deserialize(edits, scala.io.Source.fromFile(file).getLines.toList.mkString("").getBytes)
-      edits.edits
-    })
-
+    if (dir.exists && dir.isDirectory) {
+      allEdits = dir.listFiles.filter(f => f.getName.endsWith(".json")).flatMap(file => {
+        val edits = new RawGeocodeServingFeatureEdits
+        deserializer.deserialize(edits, scala.io.Source.fromFile(file).getLines().toList.mkString("").getBytes)
+        edits.edits
+      })
+    } else {
+      logger.warn("invalid hotfix directory: %s".format(path))
+    }
   }
 
   def getEdits(): Seq[GeocodeServingFeatureEdit] = allEdits
