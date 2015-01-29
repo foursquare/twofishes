2fe1422229ce7f8dc59f5c7806405a8efdd28317
Rahul Maddimsetty <rahul@foursquare.com>
2015-01-29 18:32:27 -0500

    no-brainer perf win even on single machine indexer
    we don't need to re-buffer and re-prepare the original geometry for each cell!

--- indexer/src/main/scala/S2CoveringAkkaWorkers.scala
+++ indexer/src/main/scala/S2CoveringAkkaWorkers.scala
@@ -89,6 +89,8 @@ class S2CoveringWorker extends Actor with DurationUtils with RevGeoConstants wit
         }
 
         logDuration("coverClippingForRevGeoIndex", "clipped and outputted cover for %d cells (%s) for revgeo index".format(cells.size, polyId)) {
+          val recordShape = geom.buffer(0)
+          val preparedRecordShape = PreparedGeometryFactory.prepare(recordShape)
           val records = cells.map((cellid: S2CellId) => {
             if (geom.isInstanceOf[JTSPoint]) {
               RevGeoIndex(
@@ -97,8 +99,6 @@ class S2CoveringWorker extends Actor with DurationUtils with RevGeoConstants wit
                 geom = Some(wkbWriter.write(geom))
               )
             } else {
-              val recordShape = geom.buffer(0)
-              val preparedRecordShape = PreparedGeometryFactory.prepare(recordShape)
               val s2shape = ShapefileS2Util.fullGeometryForCell(cellid)
               if (preparedRecordShape.contains(s2shape)) {
                 RevGeoIndex(cellid.id(), polyId, full = true, geom = None)
